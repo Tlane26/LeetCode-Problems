c25f4aa8d5bb9b1b3d04c8324cf3732f7848075f
Tlanetzi Chavez <chaveztlanetzi14@gmail.com>
2023-11-02 11:51:50 +0100

    Contains Duplicated leetcode 217

---
+++ ContainsDuplicated217.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+// Complejidad O(n)
+// T(n)
+bool hashSetSolution(int* nums){
+    unordered_set<int> myset;
+    
+    for(int i = 0; i < sizeof(nums) / sizeof(nums[0]); i++) {
+        if (myset.find(nums[i]) != myset.end()) {
+            cout << "Se encontró un valor duplicado: " << nums[i] << endl;
+            return 1;
+        }
+        myset.insert(nums[i]);
+    }
+    
+    cout << "No se encontraron valores duplicados." << endl;
+    return 0;
+}
+
+// Complejidad O(n log n)
+bool sortingSolution(int* nums){
+
+    vector<int> numsVec(nums, nums + (sizeof(nums) / sizeof(nums[0])));
+
+    sort(numsVec.begin(), numsVec.end());
+    for (int i = 1; i < sizeof(nums) / sizeof(nums[0]); i++) {
+        if (numsVec[i] == numsVec[i - 1])
+            return true;
+    }
+
+    return false;
+}
+
+int main(int argc, char const *argv[]) {
+    int nums[] = {1, 2, 3, 1};
+    unordered_set<int> myset;
+
+
+    // Best Solution
+    hashSetSolution(nums);
+
+    // Other solutions
+    sortingSolution(nums);
+
+
+    return 0;
+}
